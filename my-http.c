511e5e515dd6b38fd06de0e55e4f55d039ae13cd
Romwierz <miczu47@gmail.com>
2025-06-19 13:42:55 +0200

    improve basic communication with client

--- my-http.c
+++ my-http.c
@@ -1,9 +1,15 @@
+/*
+    This server can be tested with nc (netcat) utility.
+    The simplest way to connect to a server running on a localhost and port 8080 is as follows:
+    $ nc 127.0.0.1 8080
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <netinet/in.h>
 #include <errno.h>
+#include <stdbool.h>
 
 #define MY_PORT 8080
 #define LISTEN_BACKLOG 5
@@ -11,17 +17,21 @@
 #define PRINT_USAGE() fprintf(stderr, "Usage: %s [-p port]\n", argv[0])
 #define handle_error(msg) do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
+#define RECV_BUF_MAX 10
+
 const char * prompt = "> ";
+const char * helloMsg = "Hello, my name is server :-)\n";
 const char * helpMsg = "h - help\ni - request\nq - disconnect\nk - kill server\n";
 const char * msg = "HTTP 1.0 \"200 OK\"\n";
-char buf[1];
+
+char recv_buf[RECV_BUF_MAX];
 int bytesRecv;
 
 void parse_args(int argc, char *argv[]);
 
 void my_sock_init(int *my_sockfd, struct sockaddr_in *my_addr);
 void do_server_things(void);
-void send_msg(const char * ch, int client_sockfd);
+void send_msg(const char * msg, int client_sockfd);
 
 int main(int argc, char *argv[])
 {
@@ -80,8 +90,8 @@ void my_sock_init(int *my_sockfd, struct sockaddr_in *my_addr)
 
 void do_server_things(void)
 {
-    int kill_server = 0;
-    int disconnect_client = 0;
+    bool kill_server = false;
+    bool disconnect_client = false;
 
     int my_sockfd, client_sockfd;
     struct sockaddr_in my_addr, client_addr;
@@ -93,11 +103,12 @@ void do_server_things(void)
 
     while (1)
     {
-        disconnect_client = 0;
+        disconnect_client = false;
 
         if ((client_sockfd = accept(my_sockfd, (struct sockaddr *)&client_addr, &addr_size)) == -1)
             handle_error("accept");
-        printf("Found connection: client %d\n", client_sockfd);
+        printf("Found connection: client fd%d\n\n", client_sockfd);
+        send_msg(helloMsg, client_sockfd);
 
         send_msg(prompt, client_sockfd);
 
@@ -106,53 +117,53 @@ void do_server_things(void)
             switch (buf[0])
             {
             case 'h':
-                /* send help */
                 send_msg(helpMsg, client_sockfd);
-                send_msg(prompt, client_sockfd);
-                printf("Help message sent to client %d!\n", client_sockfd);
+                printf("Help message sent to client fd%d!\n\n", client_sockfd);
                 break;
             case 'i':
-                /* send msg */
                 send_msg(msg, client_sockfd);
-                send_msg(prompt, client_sockfd);
-                printf("Message sent to client %d\n", client_sockfd);
+                printf("Message sent to client fd%d\n\n", client_sockfd);
                 break;
             case 'q':
-                disconnect_client = 1;
+                disconnect_client = true;
+                printf("Disconnected with client fd%d\n\n", client_sockfd);
                 break;
             case 'k':
-                kill_server = 1;
-                printf("Server killed...\n");
-                break;
-            case '\n':
-                /* skip whitespaces */
+                kill_server = true;
+                printf("\nServer killed...\n");
                 break;
             default:
-                send_msg(helpMsg, client_sockfd);
-                send_msg(prompt, client_sockfd);
-                printf("Help message sent to client %d!\n", client_sockfd);
+                send_msg("", client_sockfd);
+                printf("Empty message sent to client fd%d\n\n", client_sockfd);
                 break;
             }
 
-            if (disconnect_client == 1) {
-                if (close(client_sockfd) != -1)
-                    printf("Disconnected with client %d\n\n", client_sockfd);
+            if (disconnect_client) {
+                if (close(client_sockfd) == -1)
+                    handle_error("close client_sockfd");
                 break;
             }
             
-            if (kill_server == 1)
+            if (kill_server)
                 break;
         }
 
-        if (kill_server == 1)
+        if (kill_server)
             break;
     }
 
-    close(my_sockfd);
+    if (close(my_sockfd) == -1)
+        handle_error("close my_sockfd");
 }
 
-void send_msg(const char * ch, int client_sockfd)
+void send_msg(const char * msg, int client_sockfd)
 {
-    if (send(client_sockfd, ch, strlen(ch), 0) == -1)
+    if (send(client_sockfd, msg, strlen(msg), 0) == -1)
         handle_error("send");
+    
+    /* send prompt only after sending the message */
+    if (send(client_sockfd, prompt, strlen(prompt), 0) == -1)
+        handle_error("send");
+
 }
+
