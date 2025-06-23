21fd64c95379a1f8a161fdab4dfccc3cc83c52de
Romwierz <miczu47@gmail.com>
2025-06-23 16:35:01 +0200

    request: split parsing request type and responsing into separate functions

--- src/request.c
+++ src/request.c
@@ -14,23 +14,40 @@ enum Cmd_request_type {
 	HELP, MESSAGE, DISCONNECT, KILL_SERVER, EMPTY
 };
 
-void handle_request(char *request, int sockfd)
+static enum Cmd_request_type parse_cmd_request(char *request)
 {
     switch (request[0])
     {
     case 'h':
+        return HELP;
+    case 'i':
+        return MESSAGE;
+    case 'q':
+        return DISCONNECT;
+    case 'k':
+        return KILL_SERVER;
+    default:
+        return EMPTY;
+    }
+}
+
+static void response(enum Request_type req_type, enum Cmd_request_type cmd_req_type, int sockfd)
+{
+    switch (cmd_req_type)
+    {
+    case HELP:
         socket_transmit(sockfd, HELP_MSG);
         printf("Help message sent to client fd%d!\n\n", sockfd);
         break;
-    case 'i':
+    case MESSAGE:
         socket_transmit(sockfd, MSG);
         printf("Message sent to client fd%d\n\n", sockfd);
         break;
-    case 'q':
+    case DISCONNECT:
         disconnect_client = true;
         printf("Disconnected with client fd%d\n\n", sockfd);
         break;
-    case 'k':
+    case KILL_SERVER:
         kill_server = true;
         printf("\nServer killed...\n");
         break;
@@ -39,4 +56,22 @@ void handle_request(char *request, int sockfd)
         printf("Empty message sent to client fd%d\n\n", sockfd);
         break;
     }
+}
+
+void handle_request(char *request, int sockfd)
+{
+    // Request_type req_type = get_req_type();
+    enum Request_type req_type = CMD;
+    enum Cmd_request_type cmd_req_type;
+
+    switch (req_type)
+    {
+    case CMD:
+        cmd_req_type = parse_cmd_request(request);
+        break;
+    default:
+        break;
+    }
+
+    response(req_type, cmd_req_type, sockfd);
 }
