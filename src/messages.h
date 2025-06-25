9a9d88b4b2272d2330271269a7062e987c35c548
Romwierz <miczu47@gmail.com>
2025-06-25 19:42:15 +0200

    messages: correct response status lines

--- src/messages.h
+++ src/messages.h
@@ -5,8 +5,8 @@
 #define HELLO_MSG   "Hello, my name is server :-)\n"
 #define HELP_MSG    "h - help\ni - request\nq - disconnect\nk - kill server\n"
 
-#define HTTP_STATUS_200 "HTTP 1.0 200 OK\n"
-#define HTTP_STATUS_400 "HTTP 1.0 400 Bad Request\n"
-#define HTTP_STATUS_404 "HTTP 1.0 404 Not Found\n"
+#define HTTP_STATUS_200 "HTTP/1.0 200 OK\r\n"
+#define HTTP_STATUS_400 "HTTP/1.0 400 Bad Request\r\n"
+#define HTTP_STATUS_404 "HTTP/1.0 404 Not Found\r\n"
 
 #endif
