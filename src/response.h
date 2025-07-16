34bc304057f953561b6236251e820cc1ed72dffa
Romwierz <miczu47@gmail.com>
2025-07-16 19:44:48 +0200

    response: define header struct outside of response struct

--- src/response.h
+++ src/response.h
@@ -1,13 +1,15 @@
 #ifndef RESPONSE_H
 #define RESPONSE_H
 
-struct Http_response_t {
-    int status_code;
     struct Header_field_t {
         struct Header_field_t *next;
         const char *name;
         char *value;
-    } header_field;
+};
+
+struct Http_response_t {
+    int status_code;
+    struct Header_field_t *headers;
     char msg_body[1024];
     long msg_body_size;
 };
