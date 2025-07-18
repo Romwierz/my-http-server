884a3886f088bd2542f497fb34890fbe9b332938
Romwierz <miczu47@gmail.com>
2025-07-18 13:32:02 +0200

    mime: get path's extension and compare it with the values from lookup table rather than searching for a substring

--- src/mime.c
+++ src/mime.c
@@ -4,12 +4,15 @@
 
 mime_t get_mime_type(const char *path)
 {
+    const char *last_dot = strrchr(path, '.');
+    const char *ext = last_dot ? last_dot + 1 : mime_type_default.ext;
+    
     const mime_t *mime_type;
 
     // iterate through mime_type_map until finding record
     // with the same ext as input path extension
     for (mime_type = mime_type_map; mime_type->type; mime_type++)
-        if (strstr(path, mime_type->ext) != NULL)
+        if (strcmp(ext, mime_type->ext) == 0)
             break;
 
     // if no match found, apply default one
