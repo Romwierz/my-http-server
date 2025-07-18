2cb0bc2c22ed929a8eb6bd03a03f622076731811
Romwierz <miczu47@gmail.com>
2025-07-18 21:23:44 +0200

    refactor: move status line string literals into response header file

--- src/response.h
+++ src/response.h
@@ -6,6 +6,16 @@
 
 #define SEND_BUF_SIZE_MAX 4096
 
+// https://datatracker.ietf.org/doc/html/rfc1945#section-9
+// https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+#define HTTP_STATUS_200 "HTTP/1.0 200 OK\r\n"
+#define HTTP_STATUS_400 "HTTP/1.0 400 Bad Request\r\n"
+#define HTTP_STATUS_403 "HTTP/1.0 403 Forbidden\r\n"
+#define HTTP_STATUS_404 "HTTP/1.0 404 Not Found\r\n"
+#define HTTP_STATUS_413 "HTTP/1.0 413 Payload Too Large\r\n"
+#define HTTP_STATUS_414 "HTTP/1.0 414 URI Too Long\r\n"
+#define HTTP_STATUS_501 "HTTP/1.0 501 Not Implemented\r\n"
+
 struct Header_field_t {
     struct Header_field_t *next;
     const char *name;
